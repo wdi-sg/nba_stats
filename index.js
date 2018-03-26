bcaf7dc7214aa953be6ba6b94ea82388c973a053
wenhao <wenhao@freeloops.local>
2018-03-26 21:03:15 +0800

    completed

---
+++ index.js
@@ -0,0 +1,53 @@
+/**
+ * ===================================
+ * Connecting to DB
+ * ===================================
+ */
+const pg = require('pg');
+const jsonfile = require('jsonfile');
+//set all configurations in an object
+const configs = {
+  user: 'wenhao',
+  host: 'localhost',
+  database: 'nba_db',
+  port: 5432,
+};
+//create new instance of client
+const client = new pg.Client(configs)
+
+/**
+ * ===================================
+ * Using the client in app
+ * ===================================
+ */
+jsonfile.readFile('players.json', (err, obj) => {
+  if (err) console.error(err);
+  // obj is all the player records
+  // what now?
+
+  client.connect((err) => {
+    if (err) {
+      console.log('error', err.message);
+    }
+
+    let data = obj.players;
+    for(let i=0;i<data.length;i++){
+      // your queries go here
+      let queryString = "INSERT INTO players (name,age,team,games,points) VALUES($1,$2,$3,$4,$5);";
+      // your dynamic values go here
+      let values = [data[i].name,data[i].age,data[i].team,data[i].games,data[i].points];
+
+      client.query(queryString, values, (err, res) => {
+        if (err) {
+          console.log('query error', err.message);
+        } else {
+          console.log('result', res.rows[0]);
+        }       
+        if(data[i] == data[data.length-1]){
+          // the last query you make, close the connection.
+          client.end();
+        }
+      });
+    }
+  });
+});
