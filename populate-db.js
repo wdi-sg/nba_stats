083beb90825d7503fe520d3ab6ddc675d14598bd
RequiemDK <kok.chee.kean@outlook.com>
2018-03-28 13:24:29 +0800

    fixed the parallel awaits :D

--- populate-db.js
+++ populate-db.js
@@ -17,7 +17,7 @@ const insertPlayer = {
     values: []
 }
 
-// async-ify the classic jsonfile.readFile method because I don't like callbacks :x
+// async-ify the classic jsonfile.readFile method because I don't like callbacks, and I still want to do this thing in parallel with the database client connection :x
 async function readJSON(f) {
     return new Promise((resolve, reject) => {
         jsonfile.readFile(f, (err, obj) => {
@@ -32,8 +32,9 @@ async function savePlayerInDB(player) {
 }
 
 async function populateDB() {
-    let fileData = readJSON(FILE);
-    await fileData + await dbClient.connect();
+    let fileData = null;
+    let readFileData = async function() {fileData = await readJSON(FILE)};
+    await readFileData() + await dbClient.connect();
     let players = fileData.players;
     let l = players.length;
     let dbTasks = [];
@@ -47,22 +48,22 @@ async function populateDB() {
 
 populateDB();
 
-const queries = ["SELECT * FROM players WHERE team = 'NYK';", 
-"SELECT * FROM players WHERE team = 'IND' AND age < 26;", 
-"SELECT * FROM players ORDER BY points;", 
-"SELECT name, points FROM players ORDER BY points DESC LIMIT 20;", "SELECT AVG(age) FROM players;", 
-"SELECT AVG(age) FROM players WHERE team = 'OKC';", 
-"SELECT AVG(age) FROM players WHERE games > 40;", 
-"SELECT team, SUM(points) AS 'Team Points' FROM players GROUP BY team ORDER BY 'Team Points' DESC;",
-"SELECT age, AVG(points / games * 1.0) FROM players GROUP BY age ORDER BY age;", 
-"SELECT team, COUNT(points / games * 1.0 > 12) AS 'Star Count' FROM players GROUP BY team ORDER BY 'Star Count' DESC;"]
+// const queries = ["SELECT * FROM players WHERE team = 'NYK';", 
+// "SELECT * FROM players WHERE team = 'IND' AND age < 26;", 
+// "SELECT * FROM players ORDER BY points;", 
+// "SELECT name, points FROM players ORDER BY points DESC LIMIT 20;", "SELECT AVG(age) FROM players;", 
+// "SELECT AVG(age) FROM players WHERE team = 'OKC';", 
+// "SELECT AVG(age) FROM players WHERE games > 40;", 
+// "SELECT team, SUM(points) AS 'Team Points' FROM players GROUP BY team ORDER BY 'Team Points' DESC;",
+// "SELECT age, AVG(points / games * 1.0) FROM players GROUP BY age ORDER BY age;", 
+// "SELECT team, COUNT(points / games * 1.0 > 12) AS 'Star Count' FROM players GROUP BY team ORDER BY 'Star Count' DESC;"]
 
-async function runQuery(i) {
-    await dbClient.connect();
-    dbClient.query(queries[i]).then(async (result) => {
-        console.log(result.rows);
-        await dbClient.end()
-    });
-}
+// async function runQuery(i) {
+//     await dbClient.connect();
+//     dbClient.query(queries[i]).then(async (result) => {
+//         console.log(result.rows);
+//         await dbClient.end()
+//     });
+// }
 
-runQuery(1);
+// runQuery(1);
