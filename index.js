8722c3f20ea0a4611c2f2bb39c3690b7cf4eb9bb
Phangster <mynamebryanphang@gmail.com>
2018-06-27 02:54:01 +0800

    Deliverables completed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const pg = require('pg');
 const jsonfile = require('jsonfile');
+const fs = require('fs');
 
 // set all of the configuration in an object
 const configs = {
@@ -15,58 +16,95 @@ const client = new pg.Client(configs);
 // =============================================================
 // COMMENTED OUT AFTER LOADING THE DATABASE
 // =============================================================
-jsonfile.readFile('players.json', (err, obj) => {
-    if (err) console.error(err);
-    // start using your client
-    client.connect((err) => {
-        if (err) {
-            console.log('error', err.message);
-        } else {
-            const players = obj.players;
-            players.forEach((player) => {
-                // your queries go here
-                let queryString = 'INSERT INTO players (name, age, team, games, points) VALUES ($1, $2, $3, $4, $5)';
-                // your dynamic values go here
-                let values = [player.name, player.age, player.team, player.games, player.points];
-                client.query(queryString, values, (err, res) => {
-                    if (err) {
-                        console.log('query error', err.message);
-                    } else {
-                        if (player === players.length - 1) {
-                            client.end();
-                        }
-                    }
-                });
-            });
-        };
-    });
-});
+// jsonfile.readFile('players.json', (err, obj) => {
+//     if (err) console.error(err);
+//     // start using your client
+//     client.connect((err) => {
+//         if (err) {
+//             console.log('error', err.message);
+//         } else {
+//             const players = obj.players;
+//             players.forEach((player) => {
+//                 // your queries go here
+//                 let queryString = 'INSERT INTO players (name, age, team, games, points) VALUES ($1, $2, $3, $4, $5)';
+//                 // your dynamic values go here
+//                 let values = [player.name, player.age, player.team, player.games, player.points];
+//                 client.query(queryString, values, (err, res) => {
+//                     if (err) {
+//                         console.log('query error', err.message);
+//                     } else {
+//                         if (player === players.length - 1) {
+//                             client.end();
+//                         }
+//                     }
+//                 });
+//             });
+//         };
+//     });
+// });
 // =============================================================
 // COMMENTED OUT AFTER LOADING THE DATABASE
 // =============================================================
 
-// start using your client
+start using your client
 
 client.connect((err) => {
 
-  if (err) {
-    console.log('error', err.message);
-  }
-
-  // your queries go here
-  let queryString = '';
-
-  // your dynamic values go here
-  let values = [];
-
-  client.query(queryString, values, (err, res) => {
     if (err) {
-      console.log('query error', err.message);
-    } else {
-      console.log('result', res.rows[0]);
+        console.log('error', err.message);
     }
-    
-    // the last query you make, close the connection.
-    client.end();
-  });
-});
+    let queryString = "SELECT * FROM players WHERE team='NYK'";
+    client.query(queryString, (err, res) => {
+        if (err) {
+            console.log('query error', err.message);
+        } else {
+        	var obj = [];
+        	res.rows.forEach((player) => {
+        		obj.push(player.name);
+        	})
+        	fs.writeFile("sql_commands.txt", 'Players who are in NYK team: ' + obj, (err)=>{console.log('err')});
+            client.end();
+        }
+    })
+})
+
+client.connect((err) => {
+
+    if (err) {
+        console.log('error', err.message);
+    }
+    let queryString = "SELECT * FROM players WHERE age < 26 AND team = 'IND'";
+    client.query(queryString, (err, res) => {
+        if (err) {
+            console.log('query error', err.message);
+        } else {
+        	var obj = [];
+        	res.rows.forEach((player) => {
+        		obj.push(player.name);
+        	})
+        	fs.writeFile("sql_commands.txt", 'Players whom are from IND team and below 26 years old:  ' + obj, (err)=>{console.log('err')});
+            client.end();
+        }
+    })
+})
+
+client.connect((err) => {
+
+    if (err) {
+        console.log('error', err.message);
+    }
+    let queryString = "SELECT points FROM players ORDER BY points ASC";
+    client.query(queryString, (err, res) => {
+        if (err) {
+            console.log('query error', err.message);
+        } else {
+        	var obj = [];
+        	res.rows.forEach((num) => {
+        		obj.push(res.rows.name);
+        		console.log(res.rows);
+        	})
+        	fs.writeFile("sql_commands.txt", 'Players rating points by ascending order: ' + obj, (err)=>{console.log('err')});
+            client.end();
+        }
+    })
+})
